b83942ad60c302cb2e436fd2000446455a3adda0
To Nguyen Thanh Vu <tntvu@TMA.COM.VN>
2018-08-09 16:35:44 +0700

    go go

--- src/renderer/utils/service-status.js
+++ src/renderer/utils/service-status.js
@@ -3,7 +3,7 @@ import xrequest from './xrequest'
 
 export function serviceStatus(base_urls, ms) {
   let url = base_urls.split(',')[0]
-  console.log(url)
+  url = url.startsWith('http') ? url : 'http://' + url
   return timeoutPromise(ms, xrequest({
     url,
     method: 'get'
